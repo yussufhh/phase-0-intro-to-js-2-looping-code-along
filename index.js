aa6d421a6abc7c99614d153af7006c3bf06a8d8f
yussufhh <0793477632g@gmail.com>
2024-05-28 12:31:52 +0300

    final-commit-for-looping-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const thankYouMessage = [];
+    for(let i = 0; i<names.length; i++){
+        thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+return thankYouMessage;
+}
+ const names = ['moon lover', 'Yusskaa', 'Hassan'];
+ const event = 'celebration day'
+ function countDown(number){
+    for(let i = number; i>=0; i--){
+        console.log(i);
+    }
+ }
